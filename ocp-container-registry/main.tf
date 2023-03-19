36e4320c6a74a398bd0df75948c6db5ae57fb0ad
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2023-03-19 10:19:33 -0700

    add to tools

---
+++ ocp-container-registry/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  cloud {
+    organization = "BCRegistry"
+    workspaces {
+      name = "ocp-container-registry-dev"
+    }
+  }
+}
+
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
+}
+
+provider "docker" {
+    registry_auth {
+      address  = "gcr.io"
+      username = "_json_key"
+      password = var.GOOGLE_CREDENTIALS
+    }
+}
+
+
+resource "docker_image" "ubuntu" {
+  name = "ubuntu:latest"
+}
+
+variable "GOOGLE_CREDENTIALS" {
+  type = string
+  default = ""
+}
