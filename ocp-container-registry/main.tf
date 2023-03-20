4a3baf5b5390d9abb84eb6f924b933a02371f5c6
Andrew Bolyachevets <Andriy.Bolyachevets@gov.bc.ca>
2023-03-20 09:49:25 -0700

    update

--- ocp-container-registry/main.tf
+++ ocp-container-registry/main.tf
@@ -1,35 +1,45 @@
 terraform {
-  cloud {
-    organization = "BCRegistry"
-    workspaces {
-      name = "ocp-container-registry-dev"
-    }
-  }
-}
-
-terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "3.0.2"
+      # version = "3.0.2"
+       version = "~> 3.0.1"
+    }
+    template = {
+      source = "hashicorp/template"
+      version = "2.2.0"
     }
   }
 }
 
+data "template_file" "gcp_json" {
+  template = "${file("docker.json")}"
+}
+
+
 provider "docker" {
-    registry_auth {
-      address  = "gcr.io"
-      username = "_json_key"
-      password = var.GOOGLE_CREDENTIALS
-    }
+  registry_auth {
+    address  = var.environment.registry
+    username = "_json_key"
+    password = "${data.template_file.gcp_json.rendered}"
+  }
 }
 
-
-resource "docker_image" "ubuntu" {
-  name = "ubuntu:latest"
+resource "docker_image" "img" {
+  for_each     = toset(var.images)
+  name = each.value
+  keep_locally = true
 }
 
-variable "GOOGLE_CREDENTIALS" {
-  type = string
-  default = ""
+resource "docker_tag" "img_tag" {
+  for_each     = toset(var.images)
+  depends_on = [docker_image.img]
+  source_image = each.value
+  target_image = "${var.environment.registry}/${var.environment.project_id}/${each.value}"
+}
+
+resource "docker_registry_image" "gcp_ubuntu" {
+  for_each     = toset(var.images)
+  depends_on = [docker_tag.img_tag]
+  name = "${var.environment.registry}/${var.environment.project_id}/${each.value}"
 }
